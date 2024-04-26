67a3bcd69351cde29e9e64ae9c7e3139e992ccd3
larissa-azuma <azumalinda29@gmail.com>
2024-04-26 16:44:23 +0000

    reviews updated

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -5,10 +5,7 @@ const ReviewCard = (props) => {
     <div className=" w-full md:w-1/3 bg-white border-2 border-lightText md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
       <div>
         <p className=" text-lightText">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-          consectetur error, dolores quae ipsa quos enim corporis magni
-          obcaecati tempore natus eos, libero ducimus nulla neque eaque maxime
-          nam molestias?
+        Stepping into SweetTooth -Pastries felt like stepping into a dreamland of pastries, and the chocolate eclair did not disappoint. The first thing that struck me was the presentation – a glossy chocolate glaze adorned with delicate swirls, enticing me to take a bite.
         </p>
       </div>
 
